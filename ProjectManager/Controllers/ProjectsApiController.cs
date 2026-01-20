488f5bca1951a46e8721f463e5a27531eb849f33
Mateusz Satala <matsatala1@gmail.com>
2026-01-20 21:28:40 +0100

    Added roles feature and user login option, created views and controlers for creating new projects and listing them, after succesfull login as admin@test.pl, Haslo123! add to url /Projects to proceed

---
+++ ProjectManager/Controllers/ProjectsApiController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectManager.Data;
+
+
+namespace ProjectManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProjectsApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProjectsApi
+        [HttpGet]
+        public async Task<IActionResult> GetProjects()
+        {
+            var projects = await _context.Projects
+                .Select(p => new { p.Id, p.Title, p.Deadline })
+                .ToListAsync();
+            return Ok(projects);
+        }
+    }
+}
--- ProjectManager/Migrations/20260120181146_AddIdentity.Designer.cs
+++ ProjectManager/Migrations/20260120181146_AddIdentity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectManager.Data;
 
@@ -11,9 +12,11 @@ using ProjectManager.Data;
 namespace ProjectManager.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260120181146_AddIdentity")]
+    partial class AddIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
