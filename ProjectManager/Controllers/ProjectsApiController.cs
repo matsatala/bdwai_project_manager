61b08344841a7a6b220c5794be2dc7477abc151d
Mateusz <matsatala1@gmail.com>
2026-01-21 14:15:39 +0100

    dodanie api i prostego zwracania listy projektów

--- ProjectManager/Controllers/ProjectsApiController.cs
+++ ProjectManager/Controllers/ProjectsApiController.cs
@@ -5,7 +5,7 @@ using ProjectManager.Data;
 
 namespace ProjectManager.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/projects")]
     [ApiController]
     public class ProjectsApiController : ControllerBase
     {
@@ -16,12 +16,18 @@ namespace ProjectManager.Controllers
             _context = context;
         }
 
-        // GET: api/ProjectsApi
+        // GET: api/projects
         [HttpGet]
         public async Task<IActionResult> GetProjects()
         {
             var projects = await _context.Projects
-                .Select(p => new { p.Id, p.Title, p.Deadline })
+                .Select(p => new { p.Id,
+                    p.Title,
+                    p.Description,
+                    Deadline = p.Deadline.ToString("yyyy-MM-dd"),
+                    CustomerName = p.Customer.CompanyName,
+                    
+                })
                 .ToListAsync();
             return Ok(projects);
         }
